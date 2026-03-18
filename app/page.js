d92e4374a79c725e8af636f356ac24af332d1117
kingelvis150888-byte <kingelvis150888@gmail.com>
2026-03-18 09:46:18 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Einkaufs Tool läuft 🚀</h1>
+      <p>Dein Projekt funktioniert jetzt.</p>
+    </main>
+  );
+}
